02bcdb3a467c390bbd7b70df893f033c86b54189
Natalya Norboeva <divonata@gmail.com>
2019-09-19 14:17:24 +0700

    store: added selectedBook property

--- src/store.js
+++ src/store.js
@@ -1,10 +1,25 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    selectedBook: null,
+  },
+  mutations: {
+    setSelectedBook(state, payload) {
+      state.selectedBook = payload;
+    },
+  },
+  actions: {
+    loadAndSetSelectedBook({ commit }, volume) {
+      if (!volume || !volume.selfLink) {
+        return Promise.reject();
+      }
+      return axios.get(volume.selfLink)
+        .then(({ data }) => commit('setSelectedBook', data));
+    },
+  },
 });
